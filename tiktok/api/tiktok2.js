42dfd32fbf56a5f17753822a1e61bc9e764839cb
AntonThomz <antonthomzz@gmail.com>
2024-11-21 22:54:06 +0700

    anton

--- tiktok/api/tiktok2.js
+++ tiktok/api/tiktok2.js
@@ -1,21 +1,17 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 export default async function handler(req, res) {
     const { url } = req.query;
-    console.log('URL received:', url);
     if (!url) {
-        console.log('Error: URL tidak diberikan');
         return res.status(400).json({ status: 'error', message: 'URL tidak diberikan.' });
     }
     try {
-        const requestUrl = `https://www.antoncodder.online/api/tiktok2?url=${encodeURIComponent(url)}`;
-        console.log('Requesting API:', requestUrl);
-        const response = await fetch(requestUrl);
-        const data = await response.json();
-        console.log('API Response:', data);
-        return res.status(200).json(data);
+        const requestUrl = `https://www.antoncodder.online/api/tiktok2`;
+        const response = await axios.post(requestUrl, {
+            url: url
+        });
+        return res.status(200).json(response.data);
     } catch (error) {
-        console.error('API Error:', error.message);
         return res.status(500).json({ status: 'error', message: 'Kesalahan server.' });
     }
 }
