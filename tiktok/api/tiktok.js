052b39eeea0c0504e9c637953e8f8f9957e5afbd
AntonThomz <antonthomzz@gmail.com>
2024-11-21 22:05:04 +0700

    anton tiktok

---
+++ tiktok/api/tiktok.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
+    const { url } = req.query;
+    if (!url) {
+        return res.status(400).json({ status: 'error', message: 'URL tidak diberikan.' });
+    }
+    try {
+        const response = await fetch(`https://www.antoncodder.online/api/tiktok?apikey=AlphaCoder03&url=${encodeURIComponent(url)}`);
+        const data = await response.json();
+        return res.status(200).json(data);
+    } catch (error) {
+        return res.status(500).json({ status: 'error', message: 'Kesalahan server.' });
+    }
+}
